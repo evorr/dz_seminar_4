5134345c87f5971ffd065df2016182ab6d0af008
Elena <9513495670@mail.ru>
2022-08-05 18:38:03 +0300

    added zadacha_29

---
+++ zadacha_29/Program.cs
@@ -0,0 +1,44 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов, заполненный
+// псевдослучайными числами и выводит их на экран.
+//1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+int[] GetArray(int num)
+{
+    Random rnd = new Random();
+    int [] array = new int[num];
+    for (int i =0; i<array.Length; i++)
+    {
+        array[i]=rnd.Next(); 
+    }
+    return array;
+}
+int [] array = GetArray(8);
+
+void WriteArray(int[] array)
+{
+    for (int i = 0; i< array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if(i!=array.Length-1)
+        {
+            Console.Write(", ");
+        }
+    }
+    
+    Console.Write(" -> [");
+    for (int i = 0; i< array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if(i!=array.Length-1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+}
+WriteArray(array);
+
+// Тоже выведет также вместо WriteArray:
+//Console.Write("{0}", string.Join(", ", array));
+//Console.Write(" -> ");
+//Console.Write("[{0}]", string.Join(", ", array));
