c629b4cc5d2bd7fa68e471062e5138da0f51312d
Elena <9513495670@mail.ru>
2022-08-05 18:33:11 +0300

    added zadacha 25

---
+++ zadacha_25/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵) 
+//2, 4 -> 16
+
+Console.Write("Введите число A:");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B:");
+int numB = Convert.ToInt32(Console.ReadLine());
+int Expon (int bas, int exp)
+{
+    int sum = 1;
+    for (int i = 1; i<=exp; i++)
+    {
+        sum=sum*bas;
+    }
+    return sum;
+}
+int result = Expon (numA, numB);
+Console.WriteLine ($"{numA}, {numB} -> {result}");
