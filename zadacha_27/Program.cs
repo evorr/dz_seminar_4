e1c6855e618a9241df26ca943f745072ee4a24be
Elena <9513495670@mail.ru>
2022-08-05 18:36:35 +0300

    added zadacha 27

---
+++ zadacha_27/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт 
+//сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+Console.Write("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+int Addition (int number)
+{
+    int sum = 0;
+    while(number>0)
+    {
+        int rem=number%10;
+        sum = sum+rem;
+        number = number/10;
+    }
+    return sum;
+}
+
+int result = Addition(num);
+Console.WriteLine ($"{num} -> {result}");
